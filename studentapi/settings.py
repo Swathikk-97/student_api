972fbbc1ad79ba2c8432c63dbf61f3dc8cfb76de
Swathi Bindu Prakash <swathiprakash@Swathis-MacBook-Air.local>
2024-11-22 13:20:32 +0530

    host added

--- studentapi/settings.py
+++ studentapi/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from os
+from dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,9 @@ SECRET_KEY = 'django-insecure-2gc&4um3*y%48wsf8t*0c*%04%c%s9@qmb73tduye7^%zod@cr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://student-api-jo4f.onrender.com'
+]
 
 
 # Application definition
@@ -80,14 +84,16 @@ WSGI_APPLICATION = 'studentapi.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studentapi_db',
-        'USER' : 'postgres',
-        'PASSWORD' : 'Swathi123!',
-        'HOST' : 'localhost',
-        'PORT' : '5433'
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'studentapi_db',
+    #     'USER' : 'postgres',
+    #     'PASSWORD' : 'Swathi123!',
+    #     'HOST' : 'localhost',
+    #     'PORT' : '5433'
+    # }
+    'default': dj_database_url.config(default='postgresql://product_management_pwqh_user:xocpWOvztuYAl5CR8vr9UYhAqVWkidtY@dpg-csu44k23esus73c6ut60-a.oregon-postgres.render.com/product_management_pwqh')
+
 }
 
 
